ff7a4bfd8a83b4c01bd838702adc96464bbf5e7a
Keely Glenn and Kory Skarbek <keely@>
2017-01-10 15:19:16 -0800

    made flashcards the same size

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,173 @@
 $(function() {
-  $("h2.java").click(function(){
+  $("h4.java").click(function(){
     $("p.java").toggle();
-    $("h2.java").toggle();
-    $(".java").toggleClass("background");
+    $(this).toggle();
 
   });
   $("p.java").click(function(){
-    $("h2.java").toggle();
-    $("p.java").toggle();
-    $(".java").toggleClass("background");
+    $("h4.java").toggle();
+    $(this).toggle();
   });
 
-  $("h2.operation").click(function(){
+  $("h4.operation").click(function(){
     $("p.operation").toggle();
-    $("h2.operation").toggle();
+    $("h4.operation").toggle();
   });
 
   $("p.operation").click(function(){
-    $("h2.operation").toggle();
+    $("h4.operation").toggle();
     $("p.operation").toggle();
   });
 
-  $("h2.variable").click(function(){
+  $("h4.variable").click(function(){
     $("p.variable").toggle();
-    $("h2.variable").toggle();
+    $("h4.variable").toggle();
   });
   $("p.variable").click(function(){
-    $("h2.variable").toggle();
+    $("h4.variable").toggle();
     $("p.variable").toggle();
   });
 
-  $("h2.vnc").click(function(){
+  $("h4.vnc").click(function(){
     $("p.vnc").toggle();
-    $("h2.vnc").toggle();
+    $("h4.vnc").toggle();
   });
   $("p.vnc").click(function(){
-    $("h2.vnc").toggle();
+    $("h4.vnc").toggle();
     $("p.vnc").toggle();
   });
 
-  $("h2.functions").click(function(){
+  $("h4.functions").click(function(){
     $("p.functions").toggle();
-    $("h2.functions").toggle();
+    $("h4.functions").toggle();
   });
   $("p.functions").click(function(){
-    $("h2.functions").toggle();
+    $("h4.functions").toggle();
     $("p.functions").toggle();
   });
 
-  $("h2.methods").click(function(){
+  $("h4.methods").click(function(){
     $("p.methods").toggle();
-    $("h2.methods").toggle();
+    $("h4.methods").toggle();
   });
   $("p.methods").click(function(){
-    $("h2.methods").toggle();
+    $("h4.methods").toggle();
     $("p.methods").toggle();
   });
 
-  $("h2.arguements").click(function(){
+  $("h4.arguements").click(function(){
     $("p.arguements").toggle();
-    $("h2.arguements").toggle();
+    $("h4.arguements").toggle();
   });
   $("p.arguements").click(function(){
-    $("h2.arguements").toggle();
+    $("h4.arguements").toggle();
     $("p.arguements").toggle();
   });
 
-  $("h2.parameters").click(function(){
+  $("h4.parameters").click(function(){
     $("p.parameters").toggle();
-    $("h2.parameters").toggle();
+    $("h4.parameters").toggle();
   });
   $("p.parameters").click(function(){
-    $("h2.parameters").toggle();
+    $("h4.parameters").toggle();
     $("p.parameters").toggle();
   });
 
-  $("h2.return").click(function(){
+  $("h4.return").click(function(){
     $("p.return").toggle();
-    $("h2.return").toggle();
+    $("h4.return").toggle();
   });
   $("p.return").click(function(){
-    $("h2.return").toggle();
+    $("h4.return").toggle();
     $("p.return").toggle();
   });
 
-  $("h2.chains").click(function(){
+  $("h4.chains").click(function(){
     $("p.chains").toggle();
-    $("h2.chains").toggle();
+    $("h4.chains").toggle();
   });
   $("p.chains").click(function(){
-    $("h2.chains").toggle();
+    $("h4.chains").toggle();
     $("p.chains").toggle();
   });
 
-  $("h2.strings").click(function(){
+  $("h4.strings").click(function(){
     $("p.strings").toggle();
-    $("h2.strings").toggle();
+    $("h4.strings").toggle();
   });
   $("p.strings").click(function(){
-    $("h2.strings").toggle();
+    $("h4.strings").toggle();
     $("p.strings").toggle();
+  });
+  $("h4.booleans").click(function(){
+    $("p.booleans").toggle();
+    $("h4.booleans").toggle();
+  });
+  $("p.booleans").click(function(){
+    $("h4.booleans").toggle();
+    $("p.booleans").toggle();
+  });
+
+  $("h4.undefined").click(function(){
+    $("p.undefined").toggle();
+    $("h4.undefined").toggle();
+  });
+  $("p.undefined").click(function(){
+    $("h4.undefined").toggle();
+    $("p.undefined").toggle();
+  });
+
+  $("h4.nan").click(function(){
+    $("p.nan").toggle();
+    $("h4.nan").toggle();
+  });
+  $("p.nan").click(function(){
+    $("h4.nan").toggle();
+    $("p.nan").toggle();
+  });
+
+  $("h4.escape").click(function(){
+    $("p.escape").toggle();
+    $("h4.escape").toggle();
+  });
+  $("p.escape").click(function(){
+    $("h4.escape").toggle();
+    $("p.escape").toggle();
+  });
+
+  $("h4.alert1").click(function(){
+    $("p.alert1").toggle();
+    $("h4.alert1").toggle();
+  });
+  $("p.alert1").click(function(){
+    $("h4.alert1").toggle();
+    $("p.alert1").toggle();
+  });
+
+  $("h4.attributes").click(function(){
+    $("p.attributes").toggle();
+    $("h4.attributes").toggle();
+  });
+  $("p.attributes").click(function(){
+    $("h4.attributes").toggle();
+    $("p.attributes").toggle();
+  });
+
+  $("h4.comments").click(function(){
+    $("p.comments").toggle();
+    $("h4.comments").toggle();
+  });
+  $("p.comments").click(function(){
+    $("h4.comments").toggle();
+    $("p.comments").toggle();
+  });
+
+  $("h4.jQuery").click(function(){
+    $("p.jQuery").toggle();
+    $("h4.jQuery").toggle();
+  });
+  $("p.jQuery").click(function(){
+    $("h4.jQuery").toggle();
+    $("p.jQuery").toggle();
   });
 });
